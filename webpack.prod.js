6479e2e3be20a3d3ca435b35d5a6b648a4efa63f
Kitketovsky <kitketovsky@gmail.com>
2022-05-15 23:40:44 +0500

    Added devtool for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,6 @@ const common = require('webpack.common');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    devtool: 'source-map'
 });
