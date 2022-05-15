3e274d118bc6d0afed7223bf47850a8f3dd251f9
Kitketovsky <kitketovsky@gmail.com>
2022-05-15 23:34:48 +0500

    Production

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './img.jpg';
-import Data from './data.xml';
-import Notes from './data.csv';
-
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
-
-    return element;
+export default () => {
+    console.log('Hello Webpack!')
 }
-
-document.body.appendChild(component());
