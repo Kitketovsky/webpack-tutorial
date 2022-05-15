61e83b84099ccb582205afa8c25a4f5a9a1a34d4
Kitketovsky <kitketovsky@gmail.com>
2022-05-15 15:59:21 +0500

    Perfomance

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = (env) => {
 
     return {
         mode: 'development',
+        devtool: "eval-cheap-module-source-map",
         entry: {
             // The same as it was in optimization, but it gives you control which
             // dependencies to put in vendor
@@ -21,12 +22,36 @@ module.exports = (env) => {
                 title: 'Caching'
             })
         ],
+        // Certain utilities, plugins, and loaders only make sense when building
+        // for production. For example, it usually doesn't make sense to minify and mangle your
+        // code with the TerserPlugin while in development. These tools should
+        // typically be excluded in development:
+
+        // TerserPlugin
+        // [fullhash]/[chunkhash]/[contenthash]
+        // AggressiveSplittingPlugin
+        // AggressiveMergingPlugin
+        // ModuleConcatenationPlugin
         output: {
-            filename: '[name].[contenthash].js',
+            filename: '[name].bundle.js',
             path: path.resolve(__dirname, 'dist'),
             clean: true,
-            publicPath: '/'
+            publicPath: '/',
+            // Webpack has the ability to generate path info in the output bundle.
+            // However, this puts garbage collection pressure on projects that
+            // bundle thousands of modules. Turn this off in the options.output.pathinfo setting:
+            pathinfo: false
         },
+        optimization: {
+            runtimeChunk: 'single',
+        // Webpack does extra algorithmic work to optimize
+        // the output for size and load performance.
+        // These optimizations are performant for smaller codebases,
+        // but can be costly in larger ones:
+            removeAvailableModules: false,
+            removeEmptyChunks: false,
+            splitChunks: false
+        }
     }
 }
 
