2d0c6ef1f4092f36d6a2add672acd7678a13188b
Kitketovsky <kitketovsky@gmail.com>
2022-05-15 23:54:23 +0500

    Lazy loading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export default () => {
-    console.log('Hello Webpack!')
+function component() {
+
+    const btn = document.createElement('button');
+    btn.textContent = 'Click Me'
+
+    btn.onclick = () => import(/* webpackChunkName: 'print' */ "./print")
+        .then((module) => {
+        const print = module.default;
+
+        print();
+    });
+
+    return btn;
 }
+
+document.body.appendChild(component());
---
+++ src/print.js
@@ -0,0 +1,5 @@
+console.log("The print.js module has loaded!");
+
+export default () => {
+    console.log('Clicked on lazy loaded script')
+}
